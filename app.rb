0914855b16dcef42d3dd6932b68102e64ece4612
Rafael Lopez <lpz.raf@gmail.com>
2018-12-27 04:25:19 +0000

    Done.

--- app.rb
+++ app.rb
@@ -20,8 +20,8 @@ class App < Sinatra::Base
   end
   
   get "/multiply/:num1/:num2" do
-    @multiplication = params[:num1, :num2]
-    "#{@multiplication}."
+    @multiplication = params[:num1] * params[:num2]
+    "#{@multiplication}"
   end
 
 
